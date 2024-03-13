68a9b4b12b822179be20051427a9191c2a47bf17
Kyle Dewey <kyle.dewey@csun.edu>
2024-03-13 12:18:35 -0700

    code from class

--- src/main/java/arithmetic_exps/parser/Parser.java
+++ src/main/java/arithmetic_exps/parser/Parser.java
@@ -47,7 +47,133 @@ public class Parser {
                                      "; received: " + tokenHere.toString());
         }
     }
+
+    public ParseResult<Exp> parsePrimary(final int startPos) throws ParseException { ... }
     
+    // multexp ::= primary ((`*` | `/`) primary)*
+    public ParseResult<Exp> parseMultExp(final int startPos) throws ParseException {
+        ParseResult<Exp> retval = parsePrimary(startPos);
+        boolean shouldRun = true;
+        while (shouldRun) {
+            try {
+                final Token token = getToken(retval.nextPos);
+                final Op op = null;
+                if (token instanceof MultiplyToken) {
+                    op = new MultOp();
+                } else if (token instanceof DivToken) {
+                    op = new DivOp();
+                } else {
+                    throw new ParseException("Bad operator");
+                }
+                final ParseResult<Exp> other = parsePrimary(retval.nextPos + 1);
+                retval = new ParseResult<Exp>(new OpExp(op,
+                                                        retval.parsed,
+                                                        other.parsed),
+                                              other.nextPos);
+            } catch (final ParseException e) {
+                shouldRun = false;
+            }
+        }
+        return retval;
+    }
+    
+    // type Parser[A] = (List[Token]) => Option[(A, List[Token])]
+    //
+    // def token(expected: Token): Parser[Unit] = {
+    //   (tokens: List[Token]) => {
+    //     tokens match {
+    //       head :: tail if head == expected => Some(((), tail))
+    //       _ => None
+    //     }
+    //   }
+    // }
+
+    // val integer: Parser[Int] = {
+    //   (tokens: Token[Token]) => {
+    //     tokens match {
+    //       IntegerToken(i) :: tail => Some((i, tail))
+    //       _ => None
+    //     }
+    //   }
+    // }
+
+    // def and[A, B](p1: Parser[A], p2: Parser[B]): Parser[(A, B)] = {
+    //   (tokens1: List[Token]) => {
+    //     p1(tokens1) match {
+    //       case Some((a, tokens2)) => {
+    //         p2(tokens2) match {
+    //           case Some((b, tokens3)) => Some(((a, b), tokens3))
+    //           case _ => None
+    //         }
+    //       }
+    //       case _ => None
+    //    }
+    //  }
+    // }
+    //
+    // def or[A](p1: Parser[A], p2: Parser[A]): Parser[A] = {
+    //   (tokens1: List[Token]) => {
+    //     p1(tokens1) match {
+    //       case Some((a, rest)) => Some((a, rest))
+    //       case _ => {
+    //         p2(tokens1) match {
+    //           case Some((b, rest)) => Some((b, rest))
+    //           case None => None
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
+    //
+    // def map[A, B](p: Parser[A], f: A => B): Parser[B] = {
+    //   (tokens1: List[Token]) => {
+    //     p(tokens1) match {
+    //       case Some((a, tokens2)) => Some((f(a), tokens2))
+    //       case _ => None
+    //     }
+    //   }
+    // }
+    //
+    // def star[A](p: Parser[A]): Parser[List[A]] = ...
+    //
+    // lazy val exp: Parser[Exp] =
+    //   (integer ^^ (i => IntegerLiteralExp(i))) |
+    //   (variable ^^ (name => VariableExp(name))) |
+    //   ((and(token(LeftParenToken), // Parser[(Unit, (Unit, (Variable, (Exp, (Exp, Unit)))))] 
+    //         and(token(LetToken),
+    //             and(variable
+    //                 and(exp,
+    //                     and(exp,
+    //                         token(RightParenToken))))))
+    //     token(RightParenToken)) ^^ { case _ ~ _ ~ x ~ e1 ~ e2 ~ _ => LetExp(x, e1, e2) }) |
+    //   ((LeftParenToken ~
+    //     op ~
+    //     exp ~
+    //     exp ~
+    //     RightParenToken) ^^ { case _ ~ o ~ e1 ~ e2 ~ _ => OpExp(o, e1, e2) }) |
+    //   ((LeftParenToken ~
+    //     SingleEqualsToken ~
+    //     variable ~
+    //     exp ~
+    //     RightParenToken) ^^ { _ ~ _ ~ x ~ e ~ _ => AssignExp(x, e) })
+    
+    // def parseExp(tokens1: List[Token]): (Exp, List[Token]) = {
+    //   tokens1 match {
+    //     case IntegerLiteralToken(i) :: tokens2 => {
+    //       Some((IntegerLiteralExp(i), tokens2))
+    //     }
+    //     case LeftParenToken :: LetToken :: tokens2 => {
+    //       let (variable, tokens3) = parseVariable(tokens2)
+    //       let (initializer, tokens4) = parseExp(tokens3)
+    //       let (body, tokens5) = parseExp(tokens4)
+    //       tokens5 match {
+    //         case RightParenToken :: tokens6 => (LetExp(variable, initializer, body), tokens6)
+    //         case _ => throw ParseException(...)
+    //       }
+    //    }
+    //   for {
+    //     (token, tokens2) <- getToken(tokens)
+    //     
     public ParseResult<Exp> parseExp(final int startPos) throws ParseException {
         // NEXT TIME: complete parser (read all tokens), parser for non-s-expressions
         final Token token = getToken(startPos);
